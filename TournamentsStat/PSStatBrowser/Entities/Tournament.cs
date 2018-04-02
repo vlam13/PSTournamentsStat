0207b93c50a5337151d7d6b47588f481ef6de18f
Мельник Владимир <wladimir.melnik@gmail.com>
2018-04-03 00:55:41 +0200

    add primitive parser

---
+++ TournamentsStat/PSStatBrowser/Entities/Tournament.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSStatBrowser.Entities
+{
+    class Tournament
+    {
+        public int ID { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
--- TournamentsStat/PSStatBrowserConsole/Program.cs
+++ TournamentsStat/PSStatBrowserConsole/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PSStatBrowserConsole.Entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,72 @@ namespace PSStatBrowserConsole
 {
     class Program
     {
+        public List<Tournament> TrnmtsList { get; set; }
+
         static void Main(string[] args)
         {
+            try
+            {
+                FileStream dataFile = File.Open("History.csv", FileMode.Open);
+                StreamReader strReader = new StreamReader(dataFile);
+
+                int count = 0;
+                strReader.ReadLine();
+
+                while (!strReader.EndOfStream)
+                {
+                    var line = strReader.ReadLine();
+                    if (ParseLine(line))
+                    {
+                        count++;
+                        Console.WriteLine(line);
+                    }
+                }
+
+                Console.WriteLine($"{count} lines");
+                Console.WriteLine($"Buy In: {buyIn}");
+                Console.WriteLine($"Income: {income}");
+                Console.WriteLine($"Profit: {income + buyIn}");
+                Console.WriteLine($"Deposit: {deposit}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("File not found or wrong!");
+            }
+        }
+
+        static float buyIn = 0;
+        static float income = 0;
+        static float deposit = 0;
+
+        private static bool ParseLine(string line)
+        {
+            var dep = line.Contains("Deposit");
+
+            if (!line.Contains("Tournament") && !dep)
+                return false;
+
+            var attributes = line.Split(',');
+
+            //if (!attributes[2].Contains("Spin & Go"))
+            //    return false;
+
+            var value = attributes[5].Trim('"');
+            float res = 0;
+
+            if (float.TryParse(value, out res))
+            {
+                if (dep)
+                    deposit += res / 100;
+                else if (res < 0)
+                    buyIn += res/100;
+                else
+                    income += res/100;
+            }
+            else
+                return false;
+
+            return true;
         }
     }
 }
